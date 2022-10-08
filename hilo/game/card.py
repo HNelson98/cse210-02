9f1cec3c817ff2f1f0577cd046e93aff660542f1
Amon Brollo <amonbrollo@gmail.com>
2022-10-08 15:43:58 -0300

    🃏 Added Herry's part of the program in the card file

---
+++ hilo/game/card.py
@@ -0,0 +1,22 @@
+import random
+
+class Card():
+    """A card with a number on it within the range 1-13
+    
+    The responsibility of the card is to keep track of the value face up card.
+    Attributes:
+        value (int): The number on the face up card
+    """
+    def __init__(self):
+        """Constructs a new instance of Card with a value attribute
+        Args: 
+            self (Card): an instance of Card
+        """
+        self.value = 0
+
+    def draw_card(self):
+        """Generates a new random value
+        Args: 
+            self (Card): an instance of Card
+        """
+        self.value = random.randint(1,13)
